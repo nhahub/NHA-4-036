f3374cdbbb2745014d4a81977f4bc51451050fbd
Nada Omar <na0da2021@gmail.com>
2026-05-19 22:29:51 +0300

    commit

--- Sauce_Demo/Sauce_Demo/src/test/java/SwagLabs/LoginTests.java
+++ Sauce_Demo/Sauce_Demo/src/test/java/SwagLabs/LoginTests.java
@@ -8,34 +8,31 @@ public class LoginTests extends BaseTest {
 
 
     @Test(priority = 1)
+    public void testLoginPageShowsSwagLabsTitle(){
+        loginPage.assert_test_Login_Page_Shows_SwagLabs_Title();
+    }
+
+    @Test(priority = 2)
     public void validLoginTest() {
         loginPage.login("standard_user", "secret_sauce");
         loginPage.assert_successful_Login();
     }
 
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void invalidLoginTest() {
         loginPage.login("standard_user", "secret_sauce123");
         loginPage.assert_unsuccessful_Login();
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void invalidLoginTest_empty_username_field() {
         loginPage.login("", "secret_sauce");
         loginPage.assert_empty_username_field_Login();
     }
 
-    @Test(priority = 4)
+    @Test(priority = 5)
     public void invalidLoginTest_empty_password_field() {
         loginPage.login("standard_user", "");
         loginPage.assert_empty_password_field_Login();
     }
-
-    @Test(priority = 5)
-    public void testLoginPageShowsSwagLabsTitle(){
-        loginPage.assert_test_Login_Page_Shows_SwagLabs_Title();
-    }
-
-
 }
-
